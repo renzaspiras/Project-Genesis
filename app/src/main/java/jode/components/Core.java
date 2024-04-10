51241c1929ba75d5d6958b982e5b2eab81dd587c
renzaspiras <aspirasrenz@gmail.com>
2024-04-10 18:49:48 +0800

    NULL

--- app/src/main/java/jode/components/Core.java
+++
@@ -1,5 +0,0 @@
-package jode.components;
-
-public class Core {
-  
-}
--- app/src/main/java/jode/values/values.java
+++
@@ -1,8 +0,0 @@
-package jode.values;
-
-public class values {
-  public static int height = 700;
-  public static int width = 1000;
-
-  public static String title = "jode | [~]";
-}
